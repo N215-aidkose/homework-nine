325d8eb62b4ac334da6b50055686d577f4cfc492
Aiden Kosegi <aidkose@iu.edu>
2022-04-05 10:49:01 -0400

    added notes

--- model/model.js
+++ model/model.js
@@ -1,11 +1,10 @@
-// *Think of exporting as making something public
+// THIS INFORMATION SIMULATES GETTING DATA FROM A DATABASE
 var homeContent = `<h1>HOME</h1>`;
 var aboutContent = `<h1>ABOUT</h1>`;
 var productsContent = `<h1>PRODUCTS</h1>`;
 var contactContent = `<h1>CONTACT</h1>`;
 
-export var myName = "Aiden Kosegi";
-
+// THIS FUNCTION IS BEING CALLED BY app.js which has the page name
 export function modelPageName(pgName) {
   console.log(pgName);
 
