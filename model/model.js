a2886703781a28e6a5d716b1c51905c69a265d4d
Aiden Kosegi <aidkose@iu.edu>
2022-04-11 17:24:05 -0400

    init repo

--- model/model.js
+++ model/model.js
@@ -1,6 +1,7 @@
 // THIS INFORMATION SIMULATES GETTING DATA FROM A DATABASE
 var homeContent = `<h1>HOME</h1>`;
-var aboutContent = `<h1>ABOUT</h1>`;
+var aboutContent = `<h1>ABOUT<button>PRESS</button><a href="#">ABOUT BUTTON</a></h1>`;
+
 var productsContent = `<h1>PRODUCTS</h1>`;
 var contactContent = `<h1>CONTACT</h1>`;
 
