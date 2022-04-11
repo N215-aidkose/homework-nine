95c957b21ab35ad70d95ea21559a62f31f08aeb4
Aiden Kosegi <aidkose@iu.edu>
2022-04-11 19:13:53 -0400

    home page done

--- model/model.js
+++ model/model.js
@@ -1,9 +1,22 @@
 // THIS INFORMATION SIMULATES GETTING DATA FROM A DATABASE
-var homeContent = `<h1>HOME</h1>`;
-var aboutContent = `<h1>ABOUT<button>PRESS</button><a href="#">ABOUT BUTTON</a></h1>`;
-
-var productsContent = `<h1>PRODUCTS</h1>`;
-var contactContent = `<h1>CONTACT</h1>`;
+var homeContent = `      <div class="fullsite">
+<div class="home-content">
+  <div class="hero">
+    <h1>The Blackbriar Breakfast House</h1>
+    <p>Where great taste dwells...</p>
+  </div>
+  <div class="mission">
+    <h1>High class. High Quality.</h1>
+    <p>
+      Here at the Blackbriar Breakfast House, we offer a dining
+      experience so enjoyable, you will be coming back for seconds!
+    </p>
+  </div>
+</div>
+</div>`;
+var aboutContent = ``;
+var menuContent = ``;
+var contactContent = ``;
 
 // THIS FUNCTION IS BEING CALLED BY app.js which has the page name
 export function modelPageName(pgName) {
